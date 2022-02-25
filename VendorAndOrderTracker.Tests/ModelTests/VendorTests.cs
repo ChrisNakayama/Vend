e98b551adc4d813b40e4e2370b9cf86ba69598a5
ChrisNakayama <88105917+ChrisNakayama@users.noreply.github.com>
2022-02-24 21:56:17 -0800

    Add Project structure.

---
+++ VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,8 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorAndOrderTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace VendorAndOrderTracker.Tests
+{
+ 
